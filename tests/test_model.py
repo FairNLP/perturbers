6a9f56db3e5f1e9c472b439e6ea57c97528fc520
baskrahmer <baskrahmer@gmail.com>
2024-03-03 11:04:19 +0100

    Update model tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,12 +1,27 @@
 from perturbers import Perturber
 
+UNPERTURBED = "Jack was passionate about rock climbing and his love for the sport was infectious to all men around him."
+PERTURBED_ORIGINAL = "Jackie was passionate about rock climbing and her love for the sport was infectious to all men around her."
+PERTURBED_SMALL = "Jack was passionate about rock climbing and her love for the sport was infectious to all men around her."
+PERTURBED_BASE = "Jacqueline was passionate about rock climbing and her love for the sport was infectious to all men around her."
+
 
 def test_perturber_model():
     model = Perturber()
 
-    unperturbed = "Jack was passionate about rock climbing and his love for the sport was infectious to all men around him."
-    perturbed = model.generate(unperturbed, "jack", "female")
+    perturbed = model.generate(UNPERTURBED, "Jack", "woman")
+    assert perturbed == PERTURBED_ORIGINAL
 
-    assert perturbed == "Jackie was passionate about rock climbing and her love for the sport was infectious to all men around her."
 
-    assert model(unperturbed)
+def test_small_perturber_model():
+    model = Perturber("fairnlp/perturber-small")
+
+    perturbed = model.generate(UNPERTURBED, "Jack", "woman")
+    assert perturbed == PERTURBED_SMALL
+
+
+def test_base_perturber_model():
+    model = Perturber("fairnlp/perturber-base")
+
+    perturbed = model.generate(UNPERTURBED, "Jack", "woman")
+    assert perturbed == PERTURBED_ORIGINAL
