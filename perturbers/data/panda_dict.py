adf9cbbb2d1bd5887e16335e4d59f42ff4224925
baskrahmer <baskrahmer@gmail.com>
2024-03-20 21:25:20 +0100

    Fix incomplete race axis

--- perturbers/data/panda_dict.py
+++ perturbers/data/panda_dict.py
@@ -3,7 +3,7 @@ from typing import Optional, List
 from datasets import concatenate_datasets, load_dataset
 
 GENDER_ATTRIBUTES = {"man", "woman", "non-binary"}
-RACE_ATTRIBUTES = {"black", "white", "hispanic", "native-american", "pacific-islander"}
+RACE_ATTRIBUTES = {"black", "white", "asian", "hispanic", "native-american", "pacific-islander"}
 AGE_ATTRIBUTES = {"child", "young", "middle-aged", "senior", "adult"}
 ALL_ATTRIBUTES = GENDER_ATTRIBUTES | RACE_ATTRIBUTES | AGE_ATTRIBUTES
 
